8dc1fa3117870e7df313566cffb96eb79d093694
KEERTHANA RAMALINGAM <keerthu.ram28@gmail.com>
2022-05-11 18:22:36 +0530

    Update for upgrade

--- scripts/upgrade_limitedCollection.js
+++ scripts/upgrade_limitedCollection.js
@@ -1,4 +1,9 @@
+const { ethers } = require("hardhat")
+
 async function main() {
+    const accounts = await ethers.provider.listAccounts();
+    console.log("Accounts", accounts[0]);
+
     const master = await ethers.getContractFactory("LCMasterV1")
     let LCMaster = await upgrades.upgradeProxy("0x8a11870DCCb2CA0b85ceC01B13B401a93c76fA9F", master)
     console.log("Your upgraded proxy is done!", LCMaster.address);
